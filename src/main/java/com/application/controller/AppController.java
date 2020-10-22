964d4696d60bb8859cda047dcd080a582b65e788
Chatterjee <516145@cognizant.com>
2020-10-22 16:46:34 +0530

    Initial Commit

---
+++ src/main/java/com/application/controller/AppController.java
@@ -0,0 +1,16 @@
+package com.application.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class AppController {
+
+    @GetMapping
+    public ResponseEntity<?> hello(){
+        return ResponseEntity.ok("Hello World...");
+    }
+}
